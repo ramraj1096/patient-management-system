870fd7c8c40f8bf74fb7b27cf94d6c7dd78daad0
ramrajb1096 <rrnayak2004@gmail.com>
2025-04-24 23:43:06 +0530

    Final commit

---
+++ patient-service/src/main/java/com/example/patientservice/controller/PatientController.java
@@ -0,0 +1,60 @@
+package com.example.patientservice.controller;
+
+
+import com.example.patientservice.dto.PatientRequestDTO;
+import com.example.patientservice.dto.PatientResponseDTO;
+import com.example.patientservice.dto.validators.CreatePatientValidationGroup;
+import com.example.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.groups.Default;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+
+    @GetMapping
+    @Operation(summary = "Get Patients")
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new Patient")
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO responseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update Existing Patient")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete the Existing Patient")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ readme.md
@@ -0,0 +1,66 @@
+# Patient Service
+
+---
+
+## Environment Variables
+
+```
+JAVA_TOOL_OPTIONS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005;
+SPRING_DATASOURCE_PASSWORD=password;
+SPRING_DATASOURCE_URL=jdbc:postgresql://patient-service-db:5432/db;
+SPRING_DATASOURCE_USERNAME=admin_user;
+SPRING_JPA_HIBERNATE_DDL_AUTO=update;
+SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092;
+SPRING_SQL_INIT_MODE=always
+```
+
+
+# Patient Service
+
+---
+
+## Environment Variables (complete list)
+```bash
+BILLING_SERVICE_ADDRESS=billing-service;
+BILLING_SERVICE_GRPC_PORT=9005;
+JAVA_TOOL_OPTIONS=-agentlib:jdwp\=transport\=dt_socket,server\=y,suspend\=n,address\=*:5005;
+SPRING_DATASOURCE_PASSWORD=password;
+SPRING_DATASOURCE_URL=jdbc:postgresql://patient-service-db:5432/db;
+SPRING_DATASOURCE_USERNAME=admin_user;
+SPRING_JPA_HIBERNATE_DDL_AUTO=update;
+SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092;
+SPRING_SQL_INIT_MODE=always
+```
+# Notification Service
+
+---
+
+## Environment Vars
+
+```
+SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092
+```
+
+## Kafka Container
+
+
+```
+KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092,EXTERNAL://localhost:9094;KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER;KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka:9093;KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT,PLAINTEXT:PLAINTEXT;KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093,EXTERNAL://:9094;KAFKA_CFG_NODE_ID=0;KAFKA_CFG_PROCESS_ROLES=controller,broker
+```
+
+## Kafka Producer Setup (Patient Service)
+
+```
+spring.kafka.consumer.key-deserializer=org.apache.kafka.common.serialization.StringDeserializer
+spring.kafka.consumer.value-deserializer=org.apache.kafka.common.serialization.ByteArrayDeserializer
+```
+
+## Environment Variables (DB)
+
+```
+SPRING_DATASOURCE_PASSWORD=password
+SPRING_DATASOURCE_URL=jdbc:postgresql://auth-service-db:5432/db
+SPRING_DATASOURCE_USERNAME=admin_user
+SPRING_JPA_HIBERNATE_DDL_AUTO=update
+SPRING_SQL_INIT_MODE=always
+```
